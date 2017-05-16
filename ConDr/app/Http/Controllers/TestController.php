c16fb9eb550693b2aa1edab29456d45741a02364
georgianaursachi <georgiana.ursachi08@gmail.com>
2017-05-16 13:45:40 +0300

    Update TestController.php

--- ConDr/app/Http/Controllers/TestController.php
+++ ConDr/app/Http/Controllers/TestController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
 class TestController extends Controller{
-    
-    public function test(){
-        $users=DB::select('select * from enumber');
-       
-        return view('welcome',compact('enr'));
-    }
-    
-}
+  
+      		      
+      public function test(){		     
+       $enr=DB::select('select name from enumber');		 
+         		         
+       return view('posts.welcome',compact('enr'));		 
+      }		      
+      		      
+  }
+
