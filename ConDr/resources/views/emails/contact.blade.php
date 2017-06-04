3e6947550f719f082b02b8951b595358b941225c
georgianaursachi <georgiana.ursachi08@gmail.com>
2017-06-04 22:50:34 +0300

    Curatenie
    Din cauza faptului am lucrat concomitent si nu am fost atente la merge
    avem niste bug-uri.
    Asa ca am ales sa stergem varianta integrata prost si sa adaugam una
    functionala.

--- ConDr/resources/views/emails/contact.blade.php
+++
@@ -1,3 +0,0 @@
-<div>
-	{{ $bodyMessage }}
-</div>
--- ConDr/resources/views/layouts/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<div id="footer">
-<!-- Copyright -->
-    <div class="copyright">
-        <ul class="menu">
-            <li>&copy; FII. All rights reserved</li><li>Design: <a href="#">GUMI</a></li>
-        </ul>
-    </div>
-
-</div>
